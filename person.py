1c3f4f61b8f49fe5b135d0d5ddc5eb35295b6803
oliviacampili <oc2273@columbia.edu>
2018-07-31 12:23:23 -0400

    the person class has properties

--- person.py
+++ person.py
@@ -1,3 +1,34 @@
 
+from flask import Flask
+from flask_restplus import Resource, Api
 
 
+# welcome to flask: http://flask.pocoo.org/
+# working with sqlalchemy & swagger:
+# http://michal.karzynski.pl/blog/2016/06/19/building-beautiful-restful-apis-using-flask-swagger-ui-flask-restplus/
+application = Flask(__name__)
+api = Api(application)
+
+
+@api.route("/api/person")                   # Create a URL route to this resource
+class Person(Resource):            # Create a RESTful resource
+    def __init__(self, colour, hobby, food, sport, animal, artist, genre, season, vacation, social_media):
+        self.colour = colour
+        self.hobby = hobby
+        self.food = food
+        self.sport = sport
+        self.animal = animal
+        self.artist = artist
+        self.genre = genre
+        self.season = season
+        self.vacation = vacation
+        self.social_media = social_media
+
+
+def main():
+    application.debug = True
+    application.run()
+
+
+if __name__ == "__main__":
+    main()
