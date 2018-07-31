71c55f457e70d3d50a88bd2b522f2bd061219c4a
oliviacampili <oc2273@columbia.edu>
2018-07-31 15:37:12 -0400

    Merge branch 'olivia-lovecalculator' of /Users/oliviacampili/PycharmProjects/python_red_jaguars with conflicts.

---
+++ match.py
@@ -0,0 +1,16 @@
+import person
+from flask import Flask
+from flask_restplus import Resource, Api
+
+
+# welcome to flask: http://flask.pocoo.org/
+# working with sqlalchemy & swagger:
+# http://michal.karzynski.pl/blog/2016/06/19/building-beautiful-restful-apis-using-flask-swagger-ui-flask-restplus/
+application = Flask(__name__)
+api = Api(application)
+
+@api.route("api/match")
+class Match(Resource):
+    def match(self):
+        for index in range(0, 10):
+
