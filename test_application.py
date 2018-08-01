b287691c4f05e298e196a07ed5795f78ebfcafcc
DanBal332 <db3260@columbia.edu>
2018-08-01 16:26:44 -0400

    hi

--- test_application.py
+++ test_application.py
@@ -17,5 +17,33 @@ class RecipeTest(TestCase):
         response2 = self.client.get('/api/recipe/Crock Pot Roast')
         self.assertTrue(200, response2.status_code)
         self.assertTrue(response2.json.get('name') == 'Crock Pot Roast')
+        response3 = self.client.get('/api/recipe/Roasted Asparagus')
+        self.assertTrue(200, response3.status_code)
+        self.assertTrue(response3.json.get('name') == 'Roasted Asparagus')
+        response4 = self.client.get('/api/recipe/Curried Lentils and Rice')
+        self.assertTrue(200, response4.status_code)
+        self.assertTrue(response4.json.get('name') == 'Curried Lentils and Rice')
+        response5 = self.client.get('/api/recipe/Big Night Pizza')
+        self.assertTrue(200, response5.status_code)
+        self.assertTrue(response5.json.get('name') == 'Big Night Pizza')
+        response6 = self.client.get("/api/recipe/Mic's Yorkshire Puds")
+        self.assertTrue(200, response6.status_code)
+        self.assertTrue(response6.json.get('name') == "Mic's Yorkshire Puds")
+        response7 = self.client.get('/api/recipe/Old-Fashioned Oatmeal Cookies')
+        self.assertTrue(200, response7.status_code)
+        self.assertTrue(response7.json.get('name') == 'Old-Fashioned Oatmeal Cookies')
+        response8 = self.client.get('/api/recipe/Blueberry Oatmeal Squares')
+        self.assertTrue(200, response8.status_code)
+        self.assertTrue(response8.json.get('name') == 'Blueberry Oatmeal Squares')
+        response9 = self.client.get('/api/recipe/Curried chicken salad')
+        self.assertTrue(200, response9.status_code)
+        self.assertTrue(response9.json.get('name') == 'Curried chicken salad')
+
+
+
+
+
+
+
 
 
